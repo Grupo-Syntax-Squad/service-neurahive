10aad371f735264ecff55acc88142d7b6b8841ef
Gabriel Reis <gabrieldreisilva4@gmail.com>
2025-03-11 09:31:04 -0300

    build: #31 inital fastapi setup

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,10 +5,13 @@ module.exports = {
             "feat", "fix", "docs", "style", "refactor",
             "perf", "test", "build", "ci", "chore", "revert"
         ]],
-        "subject-case": [2, "always", "sentence-case"],
         "header-max-length": [2, "always", 100],
         "references-empty": [2, "never"],
-        "header-pattern": [2, "always", /^(\w+):\s#\d+\s.+$/],
-        "header-correspondence": ["type", "subject"]
+    },
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\w+):\s#(\d+)\s(.+)$/,
+            headerCorrespondence: ["type", "issue", "subject"]
+        }
     }
 };
