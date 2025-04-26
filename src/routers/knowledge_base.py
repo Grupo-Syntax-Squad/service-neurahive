4891b8cfd3d2af0b2d760fbb72cd3ecead80e54b
WellingtonLFaria <wellingtonll.faria@gmail.com>
2025-04-26 09:05:07 -0300

    fix: #39 Fixed mypy and ruff errors

--- src/routers/knowledge_base.py
+++ src/routers/knowledge_base.py
@@ -6,8 +6,16 @@ from src.constants import Role
 from src.schemas.auth import CurrentUser
 from src.schemas.basic_response import BasicResponse
 from src.database.get_db import get_db
-from src.schemas.knowledge_base import PostKnowledgeBaseResponse, GetKnowledgeBaseResponse, GetKnowledgeBaseMetadataResponse
-from src.modules.knowledge_base import ReadKnowledgeBase, UploadKnowledgeBase, ListKnowledgeBases
+from src.schemas.knowledge_base import (
+    PostKnowledgeBaseResponse,
+    GetKnowledgeBaseResponse,
+    GetKnowledgeBaseMetadataResponse,
+)
+from src.modules.knowledge_base import (
+    ReadKnowledgeBase,
+    UploadKnowledgeBase,
+    ListKnowledgeBases,
+)
 
 
 router = APIRouter(prefix="/knowledge-base", tags=["Knowledge Base"])
@@ -18,10 +26,11 @@ async def upload_knowledge_base(
     file: UploadFile = File(...),
     name: str = Form(...),
     current_user: CurrentUser = Depends(Auth.get_current_user),
-    session: Session = Depends(get_db)
+    session: Session = Depends(get_db),
 ) -> PostKnowledgeBaseResponse:
     PermissionValidator(current_user, [Role.ADMIN, Role.CURATOR]).execute()
-    return await UploadKnowledgeBase(file, name, session).execute()()
+    return await UploadKnowledgeBase(file, name, session).execute()
+
 
 @router.get("/{id}", response_model=BasicResponse[GetKnowledgeBaseResponse])
 def get_knowledge_base(
@@ -30,7 +39,8 @@ def get_knowledge_base(
     current_user: CurrentUser = Depends(Auth.get_current_user),
 ) -> BasicResponse[GetKnowledgeBaseResponse]:
     PermissionValidator(current_user, [Role.ADMIN, Role.CURATOR]).execute()
-    return ReadKnowledgeBase(session, id).execute()()
+    return ReadKnowledgeBase(session, id).execute()
+
 
 @router.get("/", response_model=BasicResponse[List[GetKnowledgeBaseMetadataResponse]])
 def get_knowledge_base_metadata(
